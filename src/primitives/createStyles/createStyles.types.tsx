8a89ddfe0022ec5cb5d6cb55278cee5b4ad1f81a
alexbuicescu <alexbuicescu@gmail.com>
2018-03-20 15:50:14 +0200

    -

--- src/primitives/createStyles/createStyles.types.tsx
+++ src/primitives/createStyles/createStyles.types.tsx
@@ -3,6 +3,3 @@ import { WithStyles } from '../../utils/theme.types';
 
 export type returnType = <T extends {}>
 ( component: (React.ComponentClass<T & WithStyles> | React.ComponentType<T & WithStyles>) ) => React.ComponentClass<T & WithStyles> | React.ComponentType<T & WithStyles>
-
-// export type returnType = <T extends {}>
-// ( component: (React.ComponentClass<T & WithStyles> | React.ComponentType<T & WithStyles>) ) => any
