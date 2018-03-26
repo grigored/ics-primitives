88fdc33d24fc29136d5d5fb6d74a914a8088dec1
Grigore Danciu <grigore@instacarshare.com>
2018-03-26 19:51:54 +0300

    added storybook

---
+++ storybook/stories/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text } from '../../dist/src/primitives/Text/Text';
+
+import { storiesOf } from '@storybook/react-native';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import Button from './Button';
+import CenterView from './CenterView';
+import Welcome from './Welcome';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
+  .add('with text', () => (
+    <Button
+      onPress={action('clicked-text')}
+      title={"ASD"}
+      styles={{
+        label: {
+          backgroundColor: '#f00'
+        }
+      }}
+    />
+  ))
+  .add('with some emoji', () => (
+    <Button onPress={action('clicked-emoji')}>
+      <Text>😀 😎 👍 💯</Text>
+    </Button>
+  ));
