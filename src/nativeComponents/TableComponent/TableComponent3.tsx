b5d47979b94820ca1597e3f126d3df72193f0e40
Grigore Danciu <grigore@instacar.ro>
2018-05-04 14:26:10 +0100

    Sketch (#25)
    * sketch rendering
    
    * cleaned
    
    * table inner component works on web

---
+++ src/nativeComponents/TableComponent/TableComponent3.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View } from "../../primitives/View/View";
+
+export class TableComponent3 extends React.PureComponent<{}, {}> {
+    render() {
+        return (
+            <View>
+
+            </View>
+        )
+    }
+}
