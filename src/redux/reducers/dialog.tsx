578bd60b47ccbe03983f1cd06f45cdff7bfd4ac8
alexbuicescu <alexbuicescu@gmail.com>
2018-06-22 19:02:16 +0300

    snackbar is working

--- src/redux/reducers/dialog.tsx
+++ src/redux/reducers/dialog.tsx
@@ -2,6 +2,10 @@ import { ACTION_SHEETS_IDS } from "../../utils/enums";
 
 export enum TypeKeys {
     SHOW_ACTION_SHEET = 'instacar/dialog/SHOW_ACTION_SHEET',
+    ADD_SNACKBAR = 'instacar/dialog/ADD_SNACKBAR',
+    REMOVE_SNACKBAR = 'instacar/dialog/REMOVE_SNACKBAR',
+    REMOVE_FIRST_SNACKBAR = 'instacar/dialog/REMOVE_FIRST_SNACKBAR',
+    HIDE_SNACKBAR = 'instacar/dialog/HIDE_SNACKBAR',
 }
 
 export interface ShowActionSheetAction {
@@ -10,16 +14,47 @@ export interface ShowActionSheetAction {
     show?: boolean,
 }
 
+export interface AddSnackbarAction {
+    type: TypeKeys.ADD_SNACKBAR,
+    message: string,
+}
+
+export interface RemoveSnackbarAction {
+    type: TypeKeys.REMOVE_SNACKBAR,
+    id: number,
+}
+
+export interface HideSnackbarAction {
+    type: TypeKeys.HIDE_SNACKBAR,
+    id: number,
+}
+
+export interface RemoveFirstSnackbarAction {
+    type: TypeKeys.REMOVE_FIRST_SNACKBAR,
+}
+
+export interface Snack {
+    message: string,
+    id: number,
+    open: boolean,
+}
+
 export interface DialogState {
     sheetId?: ACTION_SHEETS_IDS,
     sheetVisible?: boolean,
+    snackbarMessages: Array<Snack>,
 }
 
 export type ActionTypes =
     | ShowActionSheetAction
+    | AddSnackbarAction
+    | RemoveSnackbarAction
+    | RemoveFirstSnackbarAction
+    | HideSnackbarAction
 
+let currentId = 0;
 
-export const dialog = (state: DialogState = {}, action: ActionTypes) => {
+export const dialog = (state: DialogState = {snackbarMessages: []}, action: ActionTypes) => {
     switch (action.type) {
         case TypeKeys.SHOW_ACTION_SHEET:
             return {
@@ -27,11 +62,41 @@ export const dialog = (state: DialogState = {}, action: ActionTypes) => {
                 sheetId: action.sheetId,
                 sheetVisible: action.show,
             };
+        case TypeKeys.ADD_SNACKBAR:
+            return {
+                ...state,
+                snackbarMessages: [
+                    ...state.snackbarMessages.map(snack => ({...snack, open: false})),
+                    {
+                        message: action.message,
+                        id: currentId++,
+                        open: true,
+                    },
+                ],
+            };
+        case TypeKeys.REMOVE_SNACKBAR:
+            return {
+                ...state,
+                snackbarMessages: state.snackbarMessages.filter(snack => snack.id !== action.id),
+            };
+        case TypeKeys.HIDE_SNACKBAR:
+            return {
+                ...state,
+                snackbarMessages: state.snackbarMessages.map(snack => ({
+                    ...snack,
+                    open: snack.id === action.id ? false : snack.open
+                })),
+            };
+        case TypeKeys.REMOVE_FIRST_SNACKBAR:
+            return {
+                ...state,
+                snackbarMessages: state.snackbarMessages.slice(1),
+            };
         default:
             return state;
     }
 
-}
+};
 
 export function showActionSheet(sheetId?: ACTION_SHEETS_IDS, show?: boolean): ShowActionSheetAction {
     return {
@@ -40,3 +105,24 @@ export function showActionSheet(sheetId?: ACTION_SHEETS_IDS, show?: boolean): Sh
         show,
     }
 }
+
+export const addSnackbar = (message: string): AddSnackbarAction => {
+    return {
+        type: TypeKeys.ADD_SNACKBAR,
+        message,
+    }
+};
+
+export const removeSnackbar = (id: number): RemoveSnackbarAction => {
+    return {
+        type: TypeKeys.REMOVE_SNACKBAR,
+        id,
+    }
+};
+
+export const hideSnackbar = (id: number): HideSnackbarAction => {
+    return {
+        type: TypeKeys.HIDE_SNACKBAR,
+        id,
+    }
+};
