f0ca4d749eba3302ef23fb8db1b5f5ff86190cdb
AlexBungiu <alex.bungiu@gmail.com>
2018-08-31 14:47:28 +0300

    Appium testing (#50)
    * added interface & function for adding appium test props;
    
    * added testId to buttons & textInputs;
    
    * added testId for web
    
    * added test props to native buttons

--- src/nativeComponents/NativeButtons/NativeButton.types.tsx
+++ src/nativeComponents/NativeButtons/NativeButton.types.tsx
@@ -1,12 +1,12 @@
-import { WithStyles } from "../..";
+import { Testable, WithStyles } from "../..";
 
 
-export interface NativeButtonOwnProps {
+export type NativeButtonOwnProps = {
     title: string,
     onPress: () => void,
     icon?: any,
     extraItemLeft?: any,
     extraItemRight?: any,
-}
+} & Testable
 
 export type NativeButtonProps = NativeButtonOwnProps & WithStyles
