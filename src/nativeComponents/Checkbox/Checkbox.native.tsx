867e210196b663b5b1c0753229762ab81218ecbc
alexbungiu <alex.bungiu@gmail.com>
2018-09-05 12:59:45 +0300

    PR changes

--- src/nativeComponents/Checkbox/Checkbox.native.tsx
+++ src/nativeComponents/Checkbox/Checkbox.native.tsx
@@ -1,49 +1,9 @@
-// import { FormControl, InputLabel } from "@material-ui/core";
 import * as React from 'react';
-import { createStyles, WithStyles } from "../..";
-import { CheckboxProps } from "./Checkbox.types";
-// import { CheckBox as NativeCheckbox } from 'react-native-elements';
-// import {CheckBox as NativeCheckbox } from 'react-native';
 
-
-const styles = () => ( {
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        minWidth: 100,
-        minHeight: 100,
-    },
-    label: {
-        flexShrink: 0,
-    },
-} );
-
-class CCheckbox extends React.PureComponent<CheckboxProps & WithStyles, {}> {
+class CCheckbox extends React.PureComponent<{}, {}> {
     render() {
         return null;
-        // let { error, onChange, title, value, classes } = this.props;
-        // return (
-        //     <FormControl error={!!error}>
-        //         <View>
-        //             {
-        //                 title &&
-        //                 <InputLabel
-        //                     classes={{
-        //                         root: classes.label as any,
-        //                     }}
-        //                 >
-        //                     {title}
-        //                 </InputLabel>
-        //             }
-        //             <CheckBox
-        //                 type={'checkbox'}
-        //                 {...( !!value ? { checked: true } : {} )}
-        //                 onChange={( value: any ) => onChange( value )}
-        //             />
-        //         </View>
-        //     </FormControl>
-        // );
     }
 }
 
-export const Checkbox = createStyles( styles, 'Checkbox' )( CCheckbox ) as React.ComponentType<CheckboxProps>;
+export const Checkbox = CCheckbox;
