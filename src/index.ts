07d54b2357aa7bdde73e2e1c14f7f9d9029f0b21
AlexBungiu <alex.bungiu@gmail.com>
2019-01-14 16:12:52 +0200

    * export TextInput & getStyleProps; (#69)

--- src/index.ts
+++ src/index.ts
@@ -8,32 +8,32 @@ declare global {
     }
 }
 
-export { createStyles } from './decorators/createStyles/createStyles';
-export { Link } from './primitives/Link/Link';
-export { Image } from './primitives/Image/Image';
-export { ScrollView } from './primitives/ScrollView/ScrollView';
-export { Text } from './primitives/Text/Text';
-export { Touchable } from './primitives/Touchable/Touchable';
-export { View } from './primitives/View/View';
-export { Form } from './redux/FormComponents/Form';
-export { FORM_INPUT_TYPES, TEXT_INPUT_TYPES, HTTP_METHOD, PLATFORM, FILTER_OPERATORS } from './utils/enums';
-export { WithStyles, Classes } from './utils/theme.types';
-export { all, appTheme, android, ios, native, web, webDesktop, webMobile, } from './utils/theme';
+export {createStyles} from './decorators/createStyles/createStyles';
+export {Link} from './primitives/Link/Link';
+export {Image} from './primitives/Image/Image';
+export {ScrollView} from './primitives/ScrollView/ScrollView';
+export {Text} from './primitives/Text/Text';
+export {Touchable} from './primitives/Touchable/Touchable';
+export {View} from './primitives/View/View';
+export {Form} from './redux/FormComponents/Form';
+export {FORM_INPUT_TYPES, TEXT_INPUT_TYPES, HTTP_METHOD, PLATFORM, FILTER_OPERATORS} from './utils/enums';
+export {WithStyles, Classes} from './utils/theme.types';
+export {all, appTheme, android, ios, native, web, webDesktop, webMobile,} from './utils/theme';
 
-export { Button, fade } from './nativeComponents/Button/Button';
-export { Dialog } from './nativeComponents/Dialog/Dialog';
-export { Snackbar } from './nativeComponents/Snackbar/Snackbar';
-export { AppContainerWeb } from './nativeComponents/AppContainerWeb/AppContainerWeb';
+export {Button, fade} from './nativeComponents/Button/Button';
+export {Dialog} from './nativeComponents/Dialog/Dialog';
+export {Snackbar} from './nativeComponents/Snackbar/Snackbar';
+export {AppContainerWeb} from './nativeComponents/AppContainerWeb/AppContainerWeb';
 // export { TextInput } from './nativeComponents/Form/TextInput/TextInput';
 // export { Form } from './nativeComponents/Form/Form';
 // export { FieldDefinition } from './nativeComponents/Form/form.types';
 
 // export { FORM_INPUT_TYPES, TEXT_INPUT_TYPES } from './utils/enums';
-export { isXs, getNestedField, getPersistStore, autoRehydrate } from './utils/common';
+export {isXs, getNestedField, getPersistStore, autoRehydrate} from './utils/common';
 
-export { apiClientMiddleware } from './redux/apiClientMiddleware';
-export { postFormDataMiddleware } from './redux/postFormDataMiddleware'
-export { s3UploadMiddleware } from './redux/s3UploadMiddleware'
+export {apiClientMiddleware} from './redux/apiClientMiddleware';
+export {postFormDataMiddleware} from './redux/postFormDataMiddleware'
+export {s3UploadMiddleware} from './redux/s3UploadMiddleware'
 
 export {
     // DisplayErrorsAction,
@@ -58,7 +58,7 @@ export {
     loadedGoogleSdk,
     validate2FA,
 } from './redux/reducers/auth';
-export { dialog, DialogState, addSnackbar, removeSnackbar } from './redux/reducers/dialog';
+export {dialog, DialogState, addSnackbar, removeSnackbar} from './redux/reducers/dialog';
 export {
     persisted,
     PersistedState,
@@ -66,10 +66,10 @@ export {
     setCodePushChecked,
     setHeaders,
 } from './redux/reducers/persisted';
-export { FieldDefinition, FormHelpersState, FormState } from './redux/FormComponents/FormComponents.types'
+export {FieldDefinition, FormHelpersState, FormState} from './redux/FormComponents/FormComponents.types'
 export * from './primitives/platform/platform';
-export { CircularProgressComponent } from './primitives/CircularProgressComponent/CircularProgressComponent'
-export { LoadingContainer } from './primitives/LoadingContainer'
+export {CircularProgressComponent} from './primitives/CircularProgressComponent/CircularProgressComponent'
+export {LoadingContainer} from './primitives/LoadingContainer'
 
 export {
     navigation,
@@ -86,23 +86,26 @@ export {
     hideAlert,
 } from './redux/reducers/navigation';
 export * from './redux/reducers/s3upload';
-export { TableComponent } from './nativeComponents/TableComponent/TableComponent';
+export {TableComponent} from './nativeComponents/TableComponent/TableComponent';
 export {
     TableDefinitionData, TableColumn, TableRowAction, TableData,
 } from './nativeComponents/TableComponent/TableComponent.types';
-export { table, setRefreshTable, loadTableData } from './redux/reducers/table';
-export { persistedTableOptions } from './redux/reducers/persistedTableOptions';
-export { TopBarButton } from './nativeComponents/TopBarButton/TopBarButton.native';
-export { requireAuthentication } from './decorators/requireAuthentication'
-export { getFormDbValue } from './redux/FormComponents/Form.utils';
-export { addTopbar } from './decorators/addTopbar/addTopbar';
-export { Alert } from './nativeComponents/Alert/Alert';
-export { PlatformStyleRules } from './utils/theme.types';
-export { Select } from './nativeComponents/Select/Select';
-export { Checkbox } from './nativeComponents/Checkbox/Checkbox';
-export { Paper } from './nativeComponents/Paper/Paper';
-export { NativeButton } from './nativeComponents/NativeButtons/NativeButton';
-export { NativeButtonsList } from './nativeComponents/NativeButtons/NativeButtonsList/NativeButtonsList';
+export {table, setRefreshTable, loadTableData} from './redux/reducers/table';
+export {persistedTableOptions} from './redux/reducers/persistedTableOptions';
+export {TopBarButton} from './nativeComponents/TopBarButton/TopBarButton.native';
+export {requireAuthentication} from './decorators/requireAuthentication'
+export {getFormDbValue} from './redux/FormComponents/Form.utils';
+export {addTopbar} from './decorators/addTopbar/addTopbar';
+export {Alert} from './nativeComponents/Alert/Alert';
+export {PlatformStyleRules} from './utils/theme.types';
+export {Select} from './nativeComponents/Select/Select';
+export {Checkbox} from './nativeComponents/Checkbox/Checkbox';
+export {Paper} from './nativeComponents/Paper/Paper';
+export {NativeButton} from './nativeComponents/NativeButtons/NativeButton';
+export {NativeButtonsList} from './nativeComponents/NativeButtons/NativeButtonsList/NativeButtonsList';
+
+export {getStyleProps} from './utils/web';
+export {TextInput} from './nativeComponents/TextInput/TextInput';
 
 export * from './utils/strings';
 export * from './utils/testUtils';
